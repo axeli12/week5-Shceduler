6dd52586100f0f45dc95b923a102174d3565d3f6
Axel Irias <axelirias@Axels-MacBook-Air.local>
2023-02-01 19:15:11 -0700

    added local

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,8 @@
-var hour = 8
+var hour = 9;
 var currentTime = $("#currentDay");
 var currentHour = dayjs().hour();
+var saveBtn = $(".saveBtn")
+var description = $(".description")
 
 
 
@@ -10,16 +12,39 @@ var currentDate = dayjs().format("dddd, MMMM D ");
 currentTime.append(currentDate);
 
 
-function itTime (){
-for (let i = 0; i < 8; i++) {
-  var  times = document.getElementById("tb"+i)
+// function itTime (){
+// for (let i = 0; i < 8; i++) {
+//   var  times = document.getElementById("tb"+i)
 
-  if (hour < currentHour){
-    times.classList.add("past")
-  }
+//   if (hour < currentHour){
+//     times.classList.add("past")
+//   } 
+//   else if (hour == currentHour){
+//     times.classList.add("present")
+//   }else {
+//     times.classList.add("fututre")
+//   }
   
-}
-};
+//   }
 
+// };
+function text(event){
+  event.preventDefault();
+
+  var des = $(this).siblings(".description").val().trim();
+
+  var savedDes = $(this).parrent().attr("id");
+
+  localStorage.setItem(savedDes, des)
+
+  saveBtn.on("click", text)
+
+  for (let i = 9; i <=5; i++){
+    var data = localStorage.getItem("hour-"+i)
+    $("#hour-"+i).children("textarea").val(data)
+ 
+  }
+}
+// itTime();
 
 
